6dff3521a4b430e6932271637774dcc8fa92637e
chavp <my.parinya@gmail.com>
2013-09-01 23:55:35 +0700

    add products
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ Web/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    [Authorize]
+    public class ProductsController : Controller
+    {
+        //
+        // GET: /Products/
+
+        public ActionResult Index()
+        {
+            var products = new List<Product>();
+            products.Add(new Product
+            {
+                Brand = "Chavp",
+                CodeName = "P-MOCKUP",
+                Name = "Hello, World.",
+                Slogan = "It's simple.",
+                Status = EProductStatus.Concept,
+            });
+            products.Add(new Product
+            {
+                Brand = "Chavp",
+                CodeName = "P-CAL",
+                Name = "ChavpCal.",
+                Slogan = "Calculate Baby",
+                Status = EProductStatus.Concept,
+            });
+
+            return View(products);
+        }
+
+    }
+}
---
+++ Web/Infrastructure/HtmlHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+public static class HtmlHelpers
+{
+    public static MvcHtmlString MenuLink(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName)
+    {
+        var currentAction = htmlHelper.ViewContext.RouteData.GetRequiredString("action");
+        var currentController = htmlHelper.ViewContext.RouteData.GetRequiredString("controller");
+        var builder = new TagBuilder("li");
+        var actionLink = htmlHelper.ActionLink(linkText, actionName, controllerName);
+        builder.InnerHtml = actionLink.ToHtmlString();
+
+        if (controllerName == currentController && actionName == currentAction)
+            builder.AddCssClass("active");
+
+        return new MvcHtmlString(builder.ToString());
+    }
+}
