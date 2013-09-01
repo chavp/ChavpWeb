6ec8d53f803d1b8dfebdc3503c0211a768ff0b2b
chavp <my.parinya@gmail.com>
2013-09-01 16:29:11 +0700

    add UI Product 1
    Signed-off-by: chavp <my.parinya@gmail.com>

--- Web/Models/Product.cs
+++ Web/Models/Product.cs
@@ -7,8 +7,11 @@ namespace Web.Models
 {
     public class Product
     {
+        public string CodeName { get; set; }
         public string Brand { get; set; }
         public string Name { get; set; }
+        public string Slogan { get; set; }
+        public EProductStatus Status { get; set; }
 
         public IList<Feature> Features { get; set; }
     }
