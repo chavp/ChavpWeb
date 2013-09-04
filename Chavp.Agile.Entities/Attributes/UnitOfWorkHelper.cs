d5be53daceef708cfa9c98a215b85e28ba4fbfd8
chavp <my.parinya@gmail.com>
2013-09-04 22:57:56 +0700

    refactor move class to UnitOfWorkHelper
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ Chavp.Agile.Entities/Attributes/UnitOfWorkHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chavp.Agile.Entities.Attributes
+{
+    public static class UnitOfWorkHelper
+    {
+        public static bool IsRepositoryMethod(MethodInfo methodInfo)
+        {
+            return IsRepositoryClass(methodInfo.DeclaringType);
+        }
+
+        public static bool IsRepositoryClass(Type type)
+        {
+            return typeof(IRepository).IsAssignableFrom(type);
+        }
+
+        public static bool HasUnitOfWorkAttribute(MethodInfo methodInfo)
+        {
+            return methodInfo.IsDefined(typeof(UnitOfWorkAttribute), true);
+        }
+    }
+}
